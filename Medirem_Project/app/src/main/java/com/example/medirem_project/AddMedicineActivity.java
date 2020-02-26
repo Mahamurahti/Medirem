e31657b6eb748f15af575eb84776a0f0ac67b10b
SallaSM <61451056+SallaSM@users.noreply.github.com>
2020-02-26 14:36:46 +0200

    Lääkelisäys kalenteri

--- Medirem_Project/app/src/main/java/com/example/medirem_project/AddMedicineActivity.java
+++ Medirem_Project/app/src/main/java/com/example/medirem_project/AddMedicineActivity.java
@@ -1,32 +1,60 @@
 package com.example.medirem_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 /**
  * Add medicine activity adds custom medicine to the list
- * @author Eric Keränen
+ * @author Eric Keränen & Salla Mikkonen
  * @version 1.1 2/2019
  */
-public class AddMedicineActivity extends AppCompatActivity {
+public class AddMedicineActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_medicine);
 
+        Button button = (Button) findViewById(R.id.openDatePicker);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new com.example.medirem_project.DatePicker();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
         // GETTING THE INTENT FROM MAIN ACTIVITY
         Intent intent = getIntent();
         String date = intent.getStringExtra(MainActivity.EXTRA_MAIN);
-
-        ((TextView)findViewById(R.id.dateView)).setText(date);
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView dateTextView = (TextView) findViewById(R.id.dateView);
+        dateTextView.setText(currentDateString);
+    }
+
 
     // UPON PRESSING THE ADD BUTTON, SENDS THE MEDICINE
     // NAME AND DESC TO THE SAVED MEDICINE LIST
@@ -49,4 +77,5 @@ public class AddMedicineActivity extends AppCompatActivity {
         setResult(0);
         finish();
     }
+
 }
