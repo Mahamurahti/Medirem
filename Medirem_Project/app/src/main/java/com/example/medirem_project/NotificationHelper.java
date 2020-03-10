78d07fab3b02fd21e3c96eef3c6577407c6c1297
Mahamurahti <dagility1@gmail.com>
2020-03-10 15:03:33 +0200

    Changed the logo, and some notification changes

--- Medirem_Project/app/src/main/java/com/example/medirem_project/NotificationHelper.java
+++ Medirem_Project/app/src/main/java/com/example/medirem_project/NotificationHelper.java
@@ -3,8 +3,10 @@ package com.example.medirem_project;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -39,9 +41,15 @@ public class NotificationHelper extends ContextWrapper {
     }
 
     public NotificationCompat.Builder getChannelNotification(String title, String message){
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         return new NotificationCompat.Builder(getApplicationContext(), channel1ID)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setSmallIcon(R.drawable.chillpilllogoround);
+                .setSmallIcon(R.drawable.chillpilllogoround)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
     }
 }
